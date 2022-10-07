d52a4308150798aa4a685021353b14af5389cc8e
ikramuddin21 <ikramuddin2122@gmail.com>
2022-10-07 22:46:09 +0600

    vertical timeline practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [timeline, setTimeline] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then(res => res.json())
+      .then(data => setTimeline(data))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content-container">
+      <div className="content-wrapper">
+        <div className="heading">
+          <h1>Roadmap</h1>
+          <p>The following roadmap lists 2022 milestones.</p>
+        </div>
+
+        <div className="timeline-area">
+          <div className="timeline-wrapper">
+            {
+              timeline.map(item => <div className="timeline" key={item.id}>
+                <div className="timeline-left">
+                  <h1>{item.timelineQuestion}</h1>
+                </div>
+                <div className={item.boxShadow ? "box-shadow timeline-right" : "timeline-right"}>
+                  <div>
+                    <h5>{item.timelineHeading}</h5>
+                    <ul>
+                      {
+                        item?.timelineItem.map((listItem, index) => <li key={index}>
+                          {listItem}
+                        </li>)
+                      }
+                    </ul>
+                  </div>
+                </div>
+              </div>)
+            }
+          </div>
+        </div>
+        <div className="footer">
+          <p>To be announced</p>
+        </div>
+      </div>
     </div>
   );
 }
